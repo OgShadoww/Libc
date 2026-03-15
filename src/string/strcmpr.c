05a0d9266d5755155067afdff6e35fce5d8a3c77
Orest <galenzao@gmail.com>
2026-03-15 17:54:25 +0000

    Delete files

--- src/string/strcmpr.c
+++
@@ -1,8 +0,0 @@
-int my_strcmpr(const unsigned char *a, const unsigned char *b) {
-  const unsigned char *a1 = a;
-  const unsigned char *b1 = b;
-  
-  while(*a1 && (*a1++ == *b1++));
-  
-  return *a1 - *b1;
-}
--- src/type/isnalum.c
+++
@@ -1,3 +0,0 @@
-int is_nalum(char c) { 
-  return (c >= 'A' && c <= 'Z' ) || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
-}
