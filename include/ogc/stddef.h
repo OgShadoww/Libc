9f06d3574c8095961e515a99f7da243ffdfdbda5
Orest <galenzao@gmail.com>
2026-03-18 20:47:55 +0000

    Change include files

--- include/ogc/stddef.h
+++ include/ogc/stddef.h
@@ -4,5 +4,4 @@
 typedef unsigned long size_t;
 typedef long ssize_t;
 
-
 #endif
--- include/ogc/sys/syscall.h
+++ include/ogc/sys/syscall.h
@@ -3,10 +3,9 @@
 
 int sys_write(int fd, char *buffer, int size);
 int sys_read(int fd, char *buffer, int size);
-int sys_open(char *path);
+int sys_open(char *path, int oflag, int mode);
 int sys_close(int fd);
 int sys_exit(int flag);
 int sys_nanosleep(int second);
-
 
 #endif
